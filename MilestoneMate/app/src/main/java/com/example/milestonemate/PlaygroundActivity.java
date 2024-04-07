265b422792440df699c9d6a0357b7740b123aa41
ThomasMoming <1569464191@qq.com>
2024-04-07 01:00:33 -0700

    check check 别一会被我玩没了

--- MilestoneMate/app/src/main/java/com/example/milestonemate/PlaygroundActivity.java
+++ MilestoneMate/app/src/main/java/com/example/milestonemate/PlaygroundActivity.java
@@ -5,10 +5,15 @@ import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -24,6 +29,10 @@ public class PlaygroundActivity extends AppCompatActivity {
     private RecyclerView giftRecyclerView;
     private RecyclerView.Adapter giftAdapter;
     private RecyclerView.LayoutManager giftLayoutManager;
+    private DrawerLayout drawerLayout;
+    private ListView drawerList;
+    private ArrayAdapter<String> drawerAdapter;
+    private Button button;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,40 +41,55 @@ public class PlaygroundActivity extends AppCompatActivity {
         db = new MyDatabase(this);
 
         characterImage = findViewById(R.id.characterImage);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        drawerList = findViewById(R.id.left_drawer);
+        button = findViewById(R.id.button);
 
         SharedPreferences sharedPreferences = getSharedPreferences("UserPrefs", MODE_PRIVATE);
         uid = sharedPreferences.getString("uid", "0");
         characterList = db.getItemsByUidAndType(uid, Constants.CHARACTER);
         giftList = db.getItemsByUidAndType(uid, Constants.GIFT);
 
-        if (characterList.size() > 0){
+        setupCharacterImage();
+        setupDrawer();
+        displayGiftList();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(drawerList);
+            }
+        });
+    }
+
+    private void setupCharacterImage() {
+        if (characterList.size() > 0) {
             String characterImagePath = db.getItemImageByName(characterList.get(0), Constants.ITEM_IMAGE_ONE);
-
             try {
-                // 获取AssetManager
                 AssetManager assetManager = getAssets();
-
-                // 从assets目录下打开图片文件流
                 InputStream is = assetManager.open(characterImagePath);
-
-                // 使用BitmapFactory从InputStream创建Bitmap
                 Bitmap bitmap = BitmapFactory.decodeStream(is);
-
-                // 设置Bitmap到ImageView
                 characterImage.setImageBitmap(bitmap);
-
-                // 关闭InputStream
                 is.close();
             } catch (IOException e) {
                 e.printStackTrace();
-                // 处理异常，可能是文件未找到等情况
             }
         }
-
-        DisplayGiftList();
     }
 
-    private void DisplayGiftList(){
+    private void setupDrawer() {
+        // 示例数据，替换为您自己的抽屉列表项
+        String[] drawerItems = new String[]{"Item 1", "Item 2", "Item 3"};
+        drawerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, drawerItems);
+        drawerList.setAdapter(drawerAdapter);
+        // 为抽屉列表项设置点击监听器
+        drawerList.setOnItemClickListener((parent, view, position, id) -> {
+            // TODO: 在这里处理点击事件
+            drawerLayout.closeDrawer(drawerList);
+        });
+    }
+
+    private void displayGiftList() {
         List<GiftSlot> giftSlots = db.getGiftListByUid(uid);
         giftRecyclerView = findViewById(R.id.giftList);
         giftRecyclerView.setHasFixedSize(true);
